efa005a50437dd7ebcbf2a9de08bcc95459e3e1e
DIEGO-OG <do904498@gmail.com>
2025-01-20 04:22:26 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✅ㅤIniciando...')
+console.log('✅ㅤIniciando...🐈‍⬛')
 import { join, dirname } from 'path'
 import { createRequire } from "module";
 import { fileURLToPath } from 'url'
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Dagos - Bot\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Diego OG`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
